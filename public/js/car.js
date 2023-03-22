9a803e0f0784ad66ed4f29df43f536f54b204c77
Nefelin <flcarmonac@yahoo.com>
2023-03-22 10:23:31 -0300

    Update

--- public/js/car.js
+++ public/js/car.js
@@ -9,6 +9,10 @@ if(cars) {
                 cars.forEach((el, elIndex) => {
                     const pos = lap.pilotos.filter(s => s.idPiloto == elIndex + 1);
                     el.style = `--i:${pos[0].idPiloto - 1};--x:${-pos[0].distancia};`
+
+                    if (!pos[0].isAlive) {
+                        el.src = pos[0].auto;
+                    };
                 });
             }, 600 * lapIndex);
         })
