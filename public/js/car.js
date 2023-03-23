cef12e5a0a2217120eaa93360c39f507144f3921
TheNefelin <metalflap@gmail.com>
2023-03-22 21:20:47 -0300

    Update

--- public/js/car.js
+++ public/js/car.js
@@ -1,20 +1,22 @@
 const cars = document.querySelectorAll(".race-speedway .race-car");
 
-if(cars) { 
+if (cars) {
     fetch("/js/simulacion.json")
-    .then(data => data.json())
-    .then(dt => {
-        dt.forEach((lap, lapIndex) => {
-            setTimeout(() => {
-                cars.forEach((el, elIndex) => {
-                    const pos = lap.pilotos.filter(s => s.idPiloto == elIndex + 1);
-                    el.style = `--i:${pos[0].idPiloto - 1};--x:${-pos[0].distancia};`
+        .then(data => data.json())
+        .then(dt => {
+            dt.forEach((lap, lapIndex) => {
 
-                    if (!pos[0].isAlive) {
-                        el.src = pos[0].auto;
-                    }
+                cars.forEach((el, elIndex) => {
+                    setTimeout(() => {
+                        const pos = lap.pilotos.filter(s => s.idPiloto == elIndex + 1);
+                        el.style = `--i:${pos[0].idPiloto - 1};--x:${-pos[0].distancia};`
+
+                        if (!pos[0].isAlive) {
+                            el.src = pos[0].auto;
+                        }
+                    }, 600 * lapIndex);
                 });
-            }, 600 * lapIndex);
-        })
-    });
+
+            })
+        });
 };
